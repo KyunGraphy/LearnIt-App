c27fd4b6748a4a0caff3020a090a5c5e3fa70c6a
KyunGraphy <onhoan0@gmail.com>
2022-07-31 11:27:29 +0700

    Connect Dataabase|Set up basic route|Set up Token

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+const authRouter = require("./routes/auth")
+
 const connectDb = async () => {
-    const DB_URL = "mongodb+srv://kien:1234@mern-learnit.royiv.mongodb.net/mern-learnit?retryWrites=true&w=majority"
+    const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-learnit.royiv.mongodb.net/mern-learnit?retryWrites=true&w=majority`
     try {
-        const db = await mongoose.connect(DB_URL, {});
+        await mongoose.connect(DB_URL, {});
+        // const db = await mongoose.connect(DB_URL, {});
 
         console.log("Connected to Mongoose database");
         // const db = await mongoose.connect(process.env.DB_URL);
@@ -18,10 +22,13 @@ const connectDb = async () => {
 connectDb();
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello, world!");
 });
+
+app.use("/api/auth", authRouter);
 
 const PORT = 5000; // port number
 
